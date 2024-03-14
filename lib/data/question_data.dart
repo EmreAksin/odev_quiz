9d0ca6b8d02305e8ac222b6278610f18c33a2fdd
Emre Aksın <devemreaksin@gmail.com>
2024-03-15 01:37:06 +0300

    created questions data

---
+++ lib/data/question_data.dart
@@ -0,0 +1,24 @@
+import 'package:odev_quiz/model/question.dart';
+
+class QuestionData {
+  // içinde question nesnesi olacak bir liste oluşturuyoruz
+  var questionList = <Question>[];
+
+  QuestionData() {
+    // nesneleri oluşturuyoruz
+    var q1 = Question(
+        question: "Soru bir",
+        answers: ["cevap1", "cevap2", "cevap3", "cevap4"]);
+    var q2 = Question(
+        question: "Soru iki",
+        answers: ["cevap1", "cevap2", "cevap3", "cevap4"]);
+    var q3 = Question(
+        question: "Soru üç", answers: ["cevap1", "cevap2", "cevap3", "cevap4"]);
+    var q4 = Question(
+        question: "Soru dört",
+        answers: ["cevap1", "cevap2", "cevap3", "cevap4"]);
+
+    // Oluşturduğumuz soru nesnelerini questionList listesine ekliyoruz.
+    questionList.addAll([q1, q2, q3, q4]);
+  }
+}
